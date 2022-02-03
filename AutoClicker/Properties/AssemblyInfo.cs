da83f5e19e437eee06d31be6763a915955c0225e
RealHoriuchi <realhoriuchi@gmail.com>
2022-02-03 00:11:25 -0500

    AssemblyInfo.cs semi revert

--- AutoClicker/Properties/AssemblyInfo.cs
+++ AutoClicker/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("A lightweight tool that automatically clicks the mouse for you.")]
+[assembly: AssemblyTitle("AutoClicker")]
 //[assembly: AssemblyDescription("")]
 //[assembly: AssemblyConfiguration("")]
 //[assembly: AssemblyCompany("")]
